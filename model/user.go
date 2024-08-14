dfd1b1247fdc07eaf8db254c2ebf086859c56c35
lifeforme <siyaga123@gmail.com>
2024-08-14 12:50:26 +0700

    datetime fixing add more passwrod in model

--- model/user.go
+++ model/user.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"encoding/json"
+
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt" // Import bcrypt package
 	"gorm.io/gorm"
 )
 
@@ -20,5 +23,31 @@ type Users struct {
 func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
  // UUID version 4
  user.ID = uuid.New()
+ hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword) // Store the hashed password
  return
+}
+// Custom JSON Marshaling for Datetime Fields
+func (u *User) MarshalJSON() ([]byte, error) {
+	type UserAlias User
+	return json.Marshal(&struct {
+		UserAlias
+		CreatedAt string `json:"CreatedAt"`
+		UpdatedAt string `json:"UpdatedAt"`
+		DeletedAt *string `json:"DeletedAt"`
+	}{
+		UserAlias: UserAlias(*u),
+		CreatedAt: u.CreatedAt.Format("2006-01-02 15:04:05"),
+		UpdatedAt: u.UpdatedAt.Format("2006-01-02 15:04:05"),
+		DeletedAt: func() *string {
+			if !u.DeletedAt.Time.IsZero() { // Access the Time field
+				formatted := u.DeletedAt.Time.Format("2006-01-02 15:04:05")
+				return &formatted // Return a pointer to the formatted string
+			}
+			return nil // Return nil for null
+		}(),
+	})
 }
